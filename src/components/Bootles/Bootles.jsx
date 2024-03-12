8ed1e73a5be9c669efb6cacec4b256c35ab69520
Noman Mia <nomanislamnishad@gmail.com>
2024-03-12 15:05:06 +0600

    first commit

---
+++ src/components/Bootles/Bootles.jsx
@@ -0,0 +1,10 @@
+
+const Bootles = () => {
+    return (
+        <div>
+            <h3>Bottles Here</h3>
+        </div>
+    );
+};
+
+export default Bootles;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Bootles from '../Bootles/Bootles';
+
+const Header = () => {
+    return (
+        <div>
+            <h4>Memorable Water Bottle</h4>
+            <Bootles></Bootles>
+        </div>
+    );
+};
+
+export default Header;
